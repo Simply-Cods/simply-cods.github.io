910275ec324ef1f81f0690f4a008166cd1c54984
Simply-Cods <79521176+Simply-Cods@users.noreply.github.com>
2022-07-30 16:41:00 +0200

    Fixed button click bug on built version

--- src/components/Info/Info.js
+++ src/components/Info/Info.js
@@ -4,6 +4,14 @@ import image from '../../images/image.png'
 import mail from '../../images/envelope-solid.svg'
 
 export default function Info() {
+    function copyEmail() {
+        const email = 'codsworthmore@gmail.com';
+    
+        navigator.clipboard.writeText(email);
+    
+        alert('Copied: ' + email)
+    }
+
     return (
         <div className='info'>
             <img className='info--img' src={image} alt='fish'/>
@@ -11,19 +19,11 @@ export default function Info() {
                 <h1 id='info--name'>Cods</h1>
                 <h3 id='info--title'>Hobbyist Developer</h3>
                 <h5 id='info--website'>simply-cods.github.com</h5> 
-                <button className='info--btn' onClick={copyEmail()} title='Copy email address'>
+                <button className='info--btn' onClick={copyEmail} title='Copy email address'>
                     <img src={mail} alt='envelope'/>
                     <p>Email</p>
                 </button>
             </div>
         </div>
     )
-}
-
-function copyEmail() {
-    const email = 'codsworthmore@gmail.com';
-
-    navigator.clipboard.writeText(email);
-
-    alert('Copied: ' + email)
 }
